b95e4723da4ef40d96858df1a43379c98783bd22
Rajveer Singh <rajsidhu2924@gmail.com>
2022-03-09 23:24:42 -0800

    function to change the color of divs by the hour made

--- develop/js/script.js
+++ develop/js/script.js
@@ -6,3 +6,26 @@ function update() {
     let today = moment();
     $("#currentDay").text(today.format("dddd, MMMM Do"));
 } setInterval(update, 1000);
+
+// change color of the divs by the hour 
+function timeBlock() {
+
+
+    $('.time-block').each(function () {
+        let rows = parseInt($(this).attr('id').split('hour')[1]);
+        console.log(currentHour, rows);
+
+        if (rows < currentHour) {
+            $(this).addClass('past')
+        }
+        else if (rows === currentHour) {
+            $(this).addClass('present')
+        }
+        else {
+            $(this).addClass('future')
+        }
+
+    })
+
+}
+timeBlock();

57f739f6fc61162a762afe2816bd2e602f3ee566
Rajveer Singh <rajsidhu2924@gmail.com>
2022-03-09 23:23:21 -0800

    function to update time made

--- develop/js/script.js
+++ develop/js/script.js
@@ -1,2 +1,8 @@
 // get the current time 
 let currentHour = moment().hour();
+
+// display todays date on top of the page 
+function update() {
+    let today = moment();
+    $("#currentDay").text(today.format("dddd, MMMM Do"));
+} setInterval(update, 1000);
